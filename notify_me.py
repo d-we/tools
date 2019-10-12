41d19b2d8046f02ce227e0b7278d813c0c133a3c
d-we <31281970+d-we@users.noreply.github.com>
2019-10-12 02:51:00 +0200

    add process attaching to notify-me

--- notify_me.py
+++ notify_me.py
@@ -19,12 +19,54 @@ def read_config(fname):
     return api_key, chat_id
 
 
+def get_cmdline(pid):
+    cmdline = ""
+    with open(f"/proc/{pid}/cmdline", "rb") as fd:
+        for line in fd:
+            # nullbyte encodes space in this file
+            cmdline += " ".join(line.decode().split("\x00"))
+    return cmdline
+
+
+def wait_for_process_exit(pid):
+    starttime = os.path.getctime(f"/proc/{pid}")
+    has_finished = False
+    while not has_finished:
+        try:
+            # we check if the creation time has changed
+            # (new process spawned with same pid)
+            new_starttime = os.path.getctime(f"/proc/{pid}")
+            if starttime != new_starttime:
+                has_finished = True
+
+            # if SIGNAL 0 works the process is still alive
+            os.kill(pid, 0)
+        except (OSError, FileNotFoundError):
+            has_finished = True
+        # wait to prevent being to noisy
+        time.sleep(3)
+
+
+def print_help():
+    print(f"[*] USAGE (command):\t{os.path.basename(sys.argv[0])} <program> <arguments>")
+    print(f"[*] USAGE (attach):\t{os.path.basename(sys.argv[0])} -p <pid>")
+
+
 def main():
     script_path = os.path.dirname(os.path.realpath(__file__))
     config_file = "notify_me.config"
     if len(sys.argv) < 2:
-        print(f"[*] USAGE: {sys.argv[0]} <program> <arguments>")
+        print_help()
         exit(0)
+
+    pid = None
+    command_mode = True
+    if sys.argv[1] == "-p":
+        if len(sys.argv) != 3:
+            print_help()
+            exit(0)
+        pid = int(sys.argv[2])
+        command_mode = False
 
     # read config file
     api_key, chat_id = read_config(script_path + "/" + config_file)
@@ -33,20 +75,28 @@ def main():
         print(f"[-] api-key: {api_key} - chat-id: {chat_id}")
         exit(0)
 
-    # treat everything as command
-    cmd = " ".join(sys.argv[1::])
+    if command_mode:
+        # treat everything as command
+        cmd = " ".join(sys.argv[1::])
 
-    # run command and measure time
-    before = time.time()
-    ret = os.system(cmd)
-    after = time.time()
+        # run command and measure time
+        before = time.time()
+        exit_code = os.system(cmd)
+        after = time.time()
+    else:
+        cmd = get_cmdline(pid)
+        print(f"[+] Attaching to process({pid}): {cmd}")
+        before = os.path.getctime(f"/proc/{pid}")
+        wait_for_process_exit(pid)
+        after = time.time()
+        exit_code = "Unknown"
 
     # format time and message
     time_elapsed_formatted = time.strftime("%H:%M:%S", time.gmtime(
         after - before))
     msg = f"Execution finished.\n" \
-        f"Command: {' '.join(sys.argv[1:])}\n" \
-        f"Exit-Code: {ret}\n" \
+        f"Command: {cmd}\n" \
+        f"Exit-Code: {exit_code}\n" \
         f"Time elapsed: {time_elapsed_formatted}."
 
     # send telegram notification
